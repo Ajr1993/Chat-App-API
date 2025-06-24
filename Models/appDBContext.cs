1c5bd17bb88744671271a8f956ff4266b2b8bd62
Ajr1993 <alexrichards10@outlook.com>
2025-06-24 18:16:33 +0100

    Add project files.

---
+++ Models/appDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chat_App_API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        }
+        public DbSet<UsersTable> Users { get; set; }
+
+
+    } 
+   
+}
+    
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Chat_App_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<AppDbContext>(delegate (DbContextOptionsBuilder DbConfiguration)
+{
+    DbConfiguration.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
